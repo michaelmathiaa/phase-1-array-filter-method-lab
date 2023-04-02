271cddcae539f8f0f8b7429c6dda3046e898f761
Michael Mathias <michaelscodingjourney@gmail.com>
2023-04-02 16:28:05 -0700

    Passed seven tests for array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+
+const findMatching = (driversNamesArray, string) => driversNamesArray.filter(driverName => driverName.toUpperCase() === string.toUpperCase());
+
+const fuzzyMatch = (driversNamesArray, string) => driversNamesArray.filter(beginningLetters => beginningLetters.startsWith(string));
+
+const matchName = (arrayOfDriverObjects, string) => arrayOfDriverObjects.filter(drivers => drivers.name === string);
